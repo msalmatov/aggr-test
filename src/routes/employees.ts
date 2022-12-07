4c84d9fc09e86235cc626d61013fae278b761c9d
Murod <almatov.ms@gmail.com>
2022-12-07 12:48:07 +0500

    little fix

--- src/routes/employees.ts
+++ src/routes/employees.ts
@@ -11,9 +11,6 @@ export default async function searchEmployees(req: Request, res: Response, next:
             result: data
         });
     } catch (err) {
-        return next({
-            error: err.message,
-            result: null
-        });
+        return next(err.message);
     }
 }
